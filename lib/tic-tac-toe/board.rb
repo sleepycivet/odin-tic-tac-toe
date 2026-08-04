eb2d5fa0cfe55f4ba77f5135932e7cecf7115305
smallerchaos <257726345+smallerchaos@users.noreply.github.com>
2026-08-04 16:48:52 +0200

    Made board logic work with an array

--- lib/tic-tac-toe/board.rb
+++ lib/tic-tac-toe/board.rb
@@ -1,31 +1,48 @@
 class Board
-  def initialize(player_1_moves, player_2_moves)
-  end  
-
   ROW_DIVIDER = "-----------"
 
-  def row_1
-    print " 1 | 2 | 3"
-    return " 1 | 2 | 3"
+  def initialize(player_1_moves, player_2_moves)
+    if player_1_moves == [] && player_2_moves == []
+      @board_spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    end 
+  end  
+
+  def row1
+    output = row_maker(@board_spaces)
+    print output
+    return output
   end
 
-  def row_div_1
+  def row_div1
     print ROW_DIVIDER
     return ROW_DIVIDER
   end
 
-  def row_2
-    print " 4 | 5 | 6"
-    return " 4 | 5 | 6"
+  def row2
+    output = row_maker(@board_spaces)
+    print output
+    return output
   end
 
-  def row_div_2
+  def row_div2
     print ROW_DIVIDER
     return ROW_DIVIDER
   end
 
-  def row_3
-    print " 7 | 8 | 9"
-    return " 7 | 8 | 9"
+  def row3
+    output = row_maker(@board_spaces)
+    print output
+    return output
   end
+
+  private
+
+  attr_reader :board_spaces
+
+  def row_maker(array)
+    row = array.slice!(0,3)
+    row = " #{row.join(" | ")}"
+    return row
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,19 +14,19 @@ RSpec.describe 'Board Class' do
   expected_row_3 = " 7 | 8 | 9"
   describe 'empty board' do
     it 'prints row 1' do
-      expect(test_board.row_1).to eq(expected_row_1)
+      expect(test_board.row1).to eq(expected_row_1)
     end
     it 'prints row divider 1' do
-      expect(test_board.row_div_1).to eq(expected_row_div_1)
+      expect(test_board.row_div1).to eq(expected_row_div_1)
     end
     it 'prints row 2' do 
-      expect(test_board.row_2).to eq(expected_row_2)
+      expect(test_board.row2).to eq(expected_row_2)
     end
     it 'prints row divider 2' do 
-      expect(test_board.row_div_2).to eq(expected_row_div_2)
+      expect(test_board.row_div2).to eq(expected_row_div_2)
     end
     it 'prints row 3' do
-      expect(test_board.row_3).to eq(expected_row_3)
+      expect(test_board.row3).to eq(expected_row_3)
     end
   end
 end
