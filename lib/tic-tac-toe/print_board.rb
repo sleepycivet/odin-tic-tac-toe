cee8fc3152a10f43eb1ec2615158d5cfa057430b
smallerchaos <257726345+smallerchaos@users.noreply.github.com>
2026-08-06 15:53:59 +0200

    Refactor print board to output it all in one line
    Discovered a way to use rspec to check output for
    one line so refactored print board to do so. Because
    of that, I also can directly access items in the
    board array via index because they will always be
    in the same order and have the same number of items.

--- lib/tic-tac-toe/print_board.rb
+++ lib/tic-tac-toe/print_board.rb
@@ -4,45 +4,18 @@ class PrintBoard
   def initialize(board_array)
     if board_array == []
       @board_spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    end 
-  end  
-
-  def row1
-    output = row_maker(@board_spaces)
-    print output
-    return output
-  end
-
-  def row_div1
-    print ROW_DIVIDER
-    return ROW_DIVIDER
-  end
-
-  def row2
-    output = row_maker(@board_spaces)
-    print output
-    return output
-  end
-
-  def row_div2
-    print ROW_DIVIDER
-    return ROW_DIVIDER
-  end
-
-  def row3
-    output = row_maker(@board_spaces)
-    print output
-    return output
+    end
+    puts " #{@board_spaces[0]} | #{@board_spaces[1]} | #{@board_spaces[2]}\n#{ROW_DIVIDER}\n #{@board_spaces[3]} | #{@board_spaces[4]} | #{@board_spaces[5]}\n#{ROW_DIVIDER}\n #{@board_spaces[6]} | #{@board_spaces[7]} | #{@board_spaces[8]}"
   end
 
   private
 
   attr_reader :board_spaces
 
-  def row_maker(array)
-    row = array.slice!(0,3)
-    row = " #{row.join(" | ")}"
-    return row
-  end
+  # def row_maker(array)
+  #   row = array.slice!(0,3)
+  #   row = " #{row.join(" | ")}"
+  #   return row
+  # end
 
 end
--- spec/print_board_spec.rb
+++ spec/print_board_spec.rb
@@ -3,27 +3,9 @@ RSpec.describe 'PrintBoard Class' do
   row_div = "-----------"
   describe 'empty board' do
     board_array = []
-    test_board = PrintBoard.new(board_array)
-
-    expected_row_1 = " 1 | 2 | 3"
-    expected_row_div_1 = row_div
-    expected_row_2 = " 4 | 5 | 6"
-    expected_row_div_2 = row_div
-    expected_row_3 = " 7 | 8 | 9"
-    it 'prints row 1' do
-      expect(test_board.row1).to eq(expected_row_1)
-    end
-    it 'prints row divider 1' do
-      expect(test_board.row_div1).to eq(expected_row_div_1)
-    end
-    it 'prints row 2' do 
-      expect(test_board.row2).to eq(expected_row_2)
-    end
-    it 'prints row divider 2' do 
-      expect(test_board.row_div2).to eq(expected_row_div_2)
-    end
-    it 'prints row 3' do
-      expect(test_board.row3).to eq(expected_row_3)
+    expected_output = " 1 | 2 | 3\n#{row_div}\n 4 | 5 | 6\n#{row_div}\n 7 | 8 | 9\n"
+    it 'prints a blank board' do
+      expect{PrintBoard.new(board_array)}.to output(expected_output).to_stdout
     end
   end
 end
