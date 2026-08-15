a55cd5c9387a71cd14398d3c28f2977fab75c261
smallerchaos <257726345+smallerchaos@users.noreply.github.com>
2026-08-15 16:45:22 +0200

    Added player move prompt mechanics and test
    Added it to Player class (rather than its own
    class) because I'm planning on storing the moves
    data in each player class.

--- lib/tic-tac-toe/player.rb
+++ lib/tic-tac-toe/player.rb
@@ -3,20 +3,37 @@ class Player
     return name.to_s
   end
 
-  def get_moves
-    return @moves
+  def prompt_move
+    input = gets
+    begin
+      input > 0
+    rescue
+      puts ERROR_MESSAGE
+    else
+      if input > 0 and input < 10
+        puts "You chose space number #{input}."
+        add_move(input)
+      else
+        puts ERROR_MESSAGE
+      end
+    end
   end
 
-  def add_move(number)
-    @moves.push(number)
+  def get_moves
+    return @moves
   end
 
   protected
   attr_reader :name
   attr_accessor :moves
+  ERROR_MESSAGE = "Please enter a number between 1 and 9."
 
   def initialize(name)
     @name = name
     @moves = []
+  end
+  
+  def add_move(number)
+    @moves.push(number)
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,6 @@
 require './lib/tic-tac-toe/player.rb'
 
-RSpec.describe 'Player Class' do
+RSpec.describe 'Player class inputs' do
   describe 'check init data' do
     test_player = Player.new('Bob Ross')
     expected_name = 'Bob Ross'
@@ -13,9 +13,45 @@ RSpec.describe 'Player Class' do
       expect(test_player.get_moves).to eq(expected_moves)
     end
 
-    it 'adds a number to the moves array' do
+    xit 'adds a number to the moves array' do # it's a protecte method now so skipping. Plus, it's test below.
       test_player.add_move(8)
       expect(test_player.get_moves).to eq([8])
+    end
+  end
+  describe 'erroneous player input' do
+    test_player = Player.new('Mr. Rogers')
+    test_input1 = "acorn"
+    test_input2 = 12345
+    expected_output = "Please enter a number between 1 and 9.\n"
+    it 'returns an error message when the user enters a word' do
+      allow_any_instance_of(Kernel).to receive(:gets).and_return(test_input1)
+      expect{test_player.prompt_move()}.to output(expected_output).to_stdout
+    end
+    it "returns an error message when the user enters a number greater than 9" do
+      allow_any_instance_of(Kernel).to receive(:gets).and_return(test_input2)
+      expect{test_player.prompt_move()}.to output(expected_output).to_stdout
+    end
+  end
+  describe 'correct player input (a number between 1 and 9)' do
+    test_player = Player.new('Ruby Nyx')
+    test_input = 3
+    expected_output = "You chose space number #{test_input}.\n"
+    it 'returns a confirmation of the move' do
+      allow_any_instance_of(Kernel).to receive(:gets).and_return(test_input)
+      expect{test_player.prompt_move()}.to output(expected_output).to_stdout
+    end
+  end
+end
+
+RSpec.describe 'Player class mechanics' do
+  describe 'add to moves to a single player' do
+    test_player = Player.new('Calaena')
+    test_move = 4
+    expected_output = [4]
+    it 'returns moves array' do
+      allow_any_instance_of(Kernel).to receive(:gets).and_return(test_move)
+      test_player.prompt_move()
+      expect(test_player.get_moves).to eq(expected_output)
     end
   end
 end
