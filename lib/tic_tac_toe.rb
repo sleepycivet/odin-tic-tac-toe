32ace97a324d93d489ac1c08147da8c32f420d59
smallerchaos <257726345+smallerchaos@users.noreply.github.com>
2026-09-02 12:33:20 +0200

    Add logic to determine if the game is a tie

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,26 +2,18 @@ require_relative 'tic-tac-toe/board'
 require_relative 'tic-tac-toe/player'
 
 class Game
-  
-  # Because I couldn't figure out how to test putting get_players in initialize (;_; which would have been the ideal situation) I am making a separate new_game function that must be called...
-  # I put an if statement in there so that people can't call #new_game while there is an active game but I'll have to see if the tests still pass
-
   def start_game
     if @active_game == false
       get_players()
+      Board.new(@player_1.get_moves, @player_2.get_moves).create_board()
       @active_game = true
 
       while @active_game == true
-        puts "#{@player_1.get_name} moves = #{@player_1.get_moves} and #{@player_2.get_name} moves = #{@player_2.get_moves}"
-        Board.new(@player_1.get_moves, @player_2.get_moves).create_board()
-
-        # TODO:// stop players from selecting a space that has already been selected
-        # TODO:// logic for what happens when there's a tie
-
         if @player_1.get_moves.length == @player_2.get_moves.length
           puts "#{@player_1.get_name}, enter a number on the board to complete your move."
 
           player_1.add_move(self.prompt_move())
+          is_tie?
           if is_win?(@player_1.get_moves)
             puts "#{@player_1.get_name} wins!"
           end
@@ -29,9 +21,47 @@ class Game
           puts "#{@player_2.get_name}, enter a number on the board to complete your move."
 
           player_2.add_move(self.prompt_move())
+          is_tie?
           if is_win?(@player_2.get_moves)
             puts "#{@player_2.get_name} wins!"
           end
+        end
+        Board.new(@player_1.get_moves, @player_2.get_moves).create_board()
+      end
+    end
+  end
+
+  protected
+  attr_accessor :player_1, :player_2, :active_game
+
+  def initialize
+    @active_game = false
+  end
+
+  def calc_remaining_moves(player_1_array, player_2_array)
+    board_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    player_1_array.each do |item|
+      board_array.delete(item)
+    end
+    player_2_array.each do |item|
+      board_array.delete(item)
+    end
+    return board_array
+  end
+
+  def prompt_move
+    error_message = "ERROR: Not a valid input. Please enter a number between 1 and 9."
+    input = gets.to_i
+    begin
+      input > 0
+    rescue
+      puts error_message
+    else
+      if input > 0 and input < 10
+        if @player_1.get_moves.include?(input) || @player_2.get_moves.include?(input)
+          puts "Another player has already gone in that area. Pick a cell with a number."
+        else
+          return input
         end
       end
     end
@@ -54,7 +84,6 @@ class Game
         end
       end
       if combo_match == true
-        Board.new(@player_1.get_moves, @player_2.get_moves).create_board()
         won = true
         @active_game = false
       end
@@ -63,37 +92,42 @@ class Game
     return won
   end
 
+  def is_tie?
+    winning_combos = [
+      [1,2,3], [4,5,6], [7,8,9], # horizontal
+      [1,4,7], [2,5,8], [3,6,9], # vertical
+      [1,5,9], [3,5,7] # diagonal
+    ]
+
+    possible_win = false
+
+    player_1_possible_moves = @player_1.get_moves + calc_remaining_moves(@player_1.get_moves, @player_2.get_moves)
+    player_2_possible_moves = @player_2.get_moves + calc_remaining_moves(@player_1.get_moves, @player_2.get_moves)
+
+    while possible_win == false && winning_combos.length > 0 do 
+      combo = winning_combos.shift()
+
+      if combo.difference(player_1_possible_moves) == []
+        possible_win = true
+      end
+
+      if combo.difference(player_2_possible_moves) == []
+        possible_win = true
+      end
+    end
+
+    if possible_win == false
+      puts "It's a tie! There are no ways for any player to have a winning move."
+      @active_game = false
+    end
+  end
+
   def get_players
     puts "Welcome to Tic-Tac-Toe."
     puts "Please enter a name for player 1."
     @player_1 = Player.new(gets.chomp.to_s)
     puts "Please enter a name for player 2."
     @player_2 = Player.new(gets.chomp.to_s)
-  end
-
-  protected
-  attr_accessor :player_1, :player_2, :active_game
-
-  def initialize
-    @active_game = false
-  end
-
-  def prompt_move
-    error_message = "ERROR: Not a valid input. Please enter a number between 1 and 9."
-    input = gets.to_i
-    begin
-      input > 0
-    rescue
-      puts error_message
-    else
-      if input > 0 and input < 10
-        if @player_1.get_moves.include?(input) || @player_2.get_moves.include?(input)
-          puts "Another player has already gone in that area. Pick a cell with a number."
-        else
-          return input
-        end
-      end
-    end
   end
 end
 
